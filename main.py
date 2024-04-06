8ad93163f8aac9a3941b818e5c43e0e8bdbbb27a
Halip26 <halipuddin.angko@gmail.com>
2024-04-06 14:37:49 +0800

    update comments

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# URI stands for Uniform Resource Indetifier
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.sqlite3"
 app.config["SECRET_KEY"] = "ABC987"
 db = SQLAlchemy(app)
 
 
-# Create a table
+# create a table
 class students(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(100))
@@ -33,6 +34,7 @@ def add_students():
         db.session.add(student)
         db.session.commit()
         return redirect(url_for("successfully"))
+
     return render_template("add_students.html")
 
 
@@ -43,7 +45,7 @@ def successfully():
 
 @app.route("/show_details")
 def show_details():
-    # Fetch all student records from the database
+    # fetch all student records from the db
     all_students = students.query.all()
     return render_template("show_details.html", students=all_students)
 
@@ -51,13 +53,14 @@ def show_details():
 @app.route("/clear_database", methods=["GET", "POST"])
 def clear_database():
     if request.method == "POST":
-        # Delete all records from the students table
+        # delete all records from the students table
         db.session.query(students).delete()
         db.session.commit()
-        return redirect(
-            url_for("successfully")
-        )  # Redirect to the page displaying the student details
-    return render_template("clear_database.html")  # Render a template for confirmation
+
+        # redirect to the show_details
+        return redirect(url_for("successfully"))
+    # render a template for confirmation
+    return render_template("clear_database.html")
 
 
 if __name__ == "__main__":
